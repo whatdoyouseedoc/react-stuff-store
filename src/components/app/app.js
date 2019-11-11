f50263cc647e9ad1fcf5885410d8dccf60a9bd03
whatdoyouseedoc <whatdoyouseedoc@gmail.com>
2019-11-11 22:46:26 +0300

    Initial preparation

---
+++ src/components/app/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './app.css';
+import { withBookstoreService } from '../hoc';
+
+const App = ({ bookstoreService }) => {
+    console.log(bookstoreService.getBooks());
+    return <div>App</div>;
+};
+
+export default withBookstoreService()(App);
---
+++ src/services/bookstore-service.js
@@ -0,0 +1,18 @@
+export default class BookstoreServvice {
+
+    getBooks() {
+        return [
+            {
+                id: 1,
+                title: 'Production-Ready Microservices',
+                author: 'Susan J. Fowler'
+            },
+            {
+                id: 2,
+                title: 'Released it!',
+                author: 'Michael T. Nygard'
+            }
+        ];
+    }
+    
+}
